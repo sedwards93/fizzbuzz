b3a6e5321129a0e2bfb247a5f3c683e0701be952
Symion Edwards <symion.edwards@gmail.com>
2020-10-20 11:35:36 +0000

    all spec tests passing

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,13 @@
 def fizzbuzz(number)
-    'fizz'
+    if  number == 0
+        0
+    elsif number % 3 == 0 && number % 5 == 0
+        'fizzbuzz'
+    elsif number % 3 == 0
+        'fizz'
+    elsif number % 5 == 0
+        'buzz'
+    else
+        number
+    end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,16 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
    expect(fizzbuzz(3)).to eq 'fizz'
  end
+  it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+end
+  it 'returns "fizzbuzz" when passed 15' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+end
+  it 'returns "4" when passed 4' do
+    expect(fizzbuzz(4)).to eq 4
+end
+  it 'returns "0" when passed 0' do
+    expect(fizzbuzz(0)).to eq 0
+end
 end
